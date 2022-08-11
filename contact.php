29c9a80b9941120fecc3bca3f9d1ca2208051077
Valentin Renon <valentin.renon@gmail.com>
2022-08-11 16:11:57 +0200

    created a dynamic  web page

---
+++ contact.php
@@ -0,0 +1,9 @@
+<?php
+$title = "Contact";
+require 'header.php';
+?>
+<h2>Contactez-nous !</h2>
+
+<p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quo ducimus nemo at facere mollitia, quibusdam magnam ratione omnis dicta magni eum est tempora atque impedit nam quia cumque! Beatae, minima?</p>
+
+<?php require 'footer.php' ?>
